946082f756e51b1f22db70ddf31ae5e5951891f0
zitao.li <zitao.li@bianlifeng.com>
2018-07-23 17:13:36 +0800

    init

---
+++ springboot-static-resource/src/main/java/com/example/springbootstaticresource/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.springbootstaticresource.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zitao.li on 2018/7/23.
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+}
